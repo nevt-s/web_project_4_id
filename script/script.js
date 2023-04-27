39161d9ef0daffabb1934bcc82c6e28dc53c8934
Steven <stevenlim.slim@gmail.com>
2023-04-27 22:55:12 +0700

    revisi pertama

--- script/script.js
+++ script/script.js
@@ -5,28 +5,30 @@ let popup = container.querySelector('.popup');
 let popupCloseButton = container.querySelector('.popup__close-btn');
 let popupName = container.querySelector('.popup__input-name');
 let popupAbout = container.querySelector('.popup__input-about');
-let popupSaveButton = container.querySelector('.popup__submit');
 let profileName = container.querySelector('.profile__name');
 let profileAbout = container.querySelector('.profile__about-me');
+let editForm = container.querySelector('.popup__input-form');
 
 function edit() {
-    popup.classList.add('popup__opened');
+    popup.classList.add('popup_opened');
     popupName.value = profileName.textContent;
     popupAbout.value = profileAbout.textContent;
 }
 
 function closePopup(){
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
     popupName.value = "";
     popupAbout.value = "";
 }
 
-function savePopup(){
-    profileName.textContent = popupName.value;
-    profileAbout.textContent = popupAbout.value;
-    closePopup();
+function savePopup(evt){
+    evt.preventDefault();
+    profileName.textContent = popupName.value; 
+    profileAbout.textContent = popupAbout.value; 
+    closePopup(); 
 }
-  
+
 editButton.addEventListener("click", edit);
 popupCloseButton.addEventListener("click", closePopup);
-popupSaveButton.addEventListener("click", savePopup);
+editForm.addEventListener("submit", savePopup);
+
